050164b94a01b86b2a1c17c722d8cd95ae1d7018
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-09-06 22:22:55 +0700

    update

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/user.js";
+import movieRoute from "./routes/movie.js";
+import listRoute from "./routes/lists.js";
 
 const app = express();
 dotenv.config();
@@ -12,7 +14,9 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute)
+app.use("/api/lists", listRoute)
 
-app.listen(8000, () => {
+app.listen(8800, () => {
     console.log("Backend server is running!");
 });
